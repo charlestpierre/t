66080d14123401936ed6d1b9123b3047525ab81e
charlestpierre <charlestpierre@gmail.com>
2023-12-12 02:21:46 -0500

    just for tests

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    if (true) {
+        music.baDing.play()
+    }
+})
 forever(function () {
     light.showAnimation(light.rainbowAnimation, 500)
 })
