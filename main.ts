81c88b7b6b9e9d304d07d837e094c5d20507d733
charlestpierre <charlestpierre@gmail.com>
2023-12-12 02:24:06 -0500

    Restore '12/12/2023, 2:18:53 AM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-    if (true) {
-        music.baDing.play()
-    }
-})
 forever(function () {
     light.showAnimation(light.rainbowAnimation, 500)
 })
